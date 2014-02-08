e75d52d4edc3e5e3f5ca8bc98483c94787163853
Nik Massie <nikolasmassie@gmail.com>
2014-02-08 17:31:57 -0500

    paste function working

--- main.js
+++ main.js
@@ -1,61 +1,47 @@
 /*jslint vars: true, plusplus: true, devel: true, nomen: true, regexp: true, indent: 4, maxerr: 50 */
-/*global define, $, brackets, window */
+/*global define, $, brackets, window, XMLHttpRequest */
 
 /** Simple extension that adds a "File > Hello World" menu item */
-/*define(function (require, exports, module) {
-    "use strict";
-    
-    
-
-    var CommandManager = brackets.getModule("command/CommandManager"),
-        Menus          = brackets.getModule("command/Menus");
-
-
-    // Function to run when the menu item is clicked
-    function handleHelloWorld() {
-        window.alert("Hello, world!");
-    }
-
-
-    // First, register a command - a UI-less object associating an id to a handler
-    var MY_COMMAND_ID = "helloworld.sayhello";   // package-style naming to avoid collisions
-    CommandManager.register("Hello World", MY_COMMAND_ID, handleHelloWorld);
-
-    // Then create a menu item bound to the command
-    // The label of the menu item is the name we gave the command (see above)
-    var menu = Menus.getMenu(Menus.AppMenuBar.FILE_MENU);
-    menu.addMenuItem(MY_COMMAND_ID);
-
-    // We could also add a key binding at the same time:
-    //menu.addMenuItem(MY_COMMAND_ID, "Ctrl-Alt-H");
-    // (Note: "Ctrl" is automatically mapped to "Cmd" on Mac)
-});*/
-
-(function () {
+define(function (require, exports, module) {
     
     'use strict';
-
+    
+    // Brackets modules
+    var CommandManager = brackets.getModule('command/CommandManager'),
+        Menus          = brackets.getModule('command/Menus');
+    
     var httpReq = new XMLHttpRequest();
     var url = 'http://paste.ee/api';
-    var fields = 'key=public&description=test&paste=this is a test paste&format=JSON';
-    var fields2 = {key: 'public', description: 'test', paste: 'this is a test paste', format: 'JSON'};
-    
-    httpReq.open('POST', url, true);
-    console.log('good');
-    
-    httpReq.setRequestHeader('Access-Control-Allow-Headers', '*');
-    httpReq.setRequestHeader('Content-type', 'application/ecmascript');
-    httpReq.setRequestHeader('Access-Control-Allow-Origin', '*');
-    console.log('ok');
-    
-    httpReq.onreadystatechange = function () {
-        console.log('wat');
-        if (httpReq.readyState === 4 && httpReq.status === 'success') {
+    var fields2 = 'key=public&description=test&paste=this is a test paste&format=JSON';
+    var fields = {key: 'public', language: 'plain', description: 'test', paste: 'this is a test paste', encrypted: 0, expire: 0, format: 'json'};
+    var fields3 = ['public', 'test', 'this is a test paste', 'JSON'];
+
+    // Pastes highlighted text 
+    var doPaste = function () {
+        httpReq.open('POST', url, true);
+        console.log('good');
+
+        //httpReq.setRequestHeader('Content-type', 'application/ecmascript');
+        httpReq.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+        console.log('ok');
+
+        httpReq.onreadystatechange = function () {
             console.log('test');
-            alert(httpReq.responseText);
-        }
+            if (httpReq.readyState === 4 && httpReq.status === 200) {
+                console.log('once');
+                console.log('test');
+                console.log(httpReq.responseText);
+            }
+        };
+        
+        httpReq.send(fields2);
     };
-    
-    httpReq.send(fields2);
-    
-}());
+
+    // Register a command (a UI-less object associating an id to a handler)
+    var commandId = 'pastePlugin.paste';
+    CommandManager.register('Upload paste', commandId, doPaste);
+
+    // Create a menu item bound to the command
+    var menu = Menus.getMenu(Menus.AppMenuBar.EDIT_MENU);
+    menu.addMenuItem(commandId, 'Ctrl-Shift-P');
+});
